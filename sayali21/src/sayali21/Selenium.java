d3c4d8ab75c9759293e6aba5ad8f3d4391002fd1
sayalibobade <sahelibobade01@gmail.com>
2024-06-13 19:27:07 +0530

    java project added

---
+++ sayali21/src/sayali21/Selenium.java
@@ -0,0 +1,15 @@
+package sayali21;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Selenium {
+
+	public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+	WebDriver driver1=new EdgeDriver();
+
+	}
+
+}
