06716e6181d61ad786ca4b9127c1b201f4e92fea
sayalibobade <sahelibobade01@gmail.com>
2024-06-18 19:09:53 +0530

    pull project added

---
+++ pullproject/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module pullproject {
+}
---
+++ pullproject/src/pullproject/Pullclass.java
@@ -0,0 +1,10 @@
+package pullproject;
+
+public class Pullclass {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}
